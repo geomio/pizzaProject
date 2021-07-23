e6016d858520df4ba1e0016afafcb38d29347230
Geoffry Millner <geoff.millner22@gmail.com>
2021-07-23 16:05:22 -0700

    Made js prototypes and objects for pricing and building pizza order project

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+//Pizza constructor
+function Pizza(size, toppings, price) {
+  this.size = size
+  this.toppings = toppings
+  this.price = price
+}
+//Prototype for pizza to find size price
+Pizza.prototype.sizePrice = function(){
+  if (this.size === 'small') {
+    this.price += 3;
+  } else if(this.size === 'medium') {
+    this.price += 6;
+  }else if (this.size === 'large') {
+    this.price += 9;
+  }
+}
+
+// Topping Object constructor
+function Topping(protein, veggie) {
+  this.protein = protein;
+  this.veggie = veggie;
+}
+// Topping price prototype for price
+Topping.prototype.toppingPrice = function(){
+  return this.protein.length * 2 + this.veggie.length;
+}
+//user object and topping below
+let userTopping = new Topping(['ham', 'chicken'], ['peppers', 'olives']);
+let userPizza = new Pizza("small", userTopping, 5);
+userPizza.sizePrice();
+userPizza.price = userPizza.price + userTopping.toppingPrice();
+console.table(userPizza);
